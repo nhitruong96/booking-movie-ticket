aea4eb86caca8beab664c05c46949a87e2cc8e49
Nhi Truong <tamnhi96@gmail.com>
2022-09-17 00:20:06 -0700

    tailwind_width_height

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import DemoGrid from './TailWindComponent/DemoGrid';
 import PaddingMarginDemo from './TailWindComponent/PaddingMarginDemo';
+import WidthHeightDemo from './TailWindComponent/WidthHeightDemo';
 
 function App() {
   return (
     <div className="App">
       {/* <DemoGrid /> */}
-      <PaddingMarginDemo />
+      {/* <PaddingMarginDemo /> */}
+      <WidthHeightDemo />
     </div>
   );
 }
