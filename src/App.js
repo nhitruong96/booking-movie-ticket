1feae5b8d2415a607241f9eec123f209f2a6849f
Nhi Truong <tamnhi96@gmail.com>
2022-09-16 23:35:07 -0700

    tailwindcss_padding_margin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import DemoGrid from './TailWindComponent/DemoGrid';
+import PaddingMarginDemo from './TailWindComponent/PaddingMarginDemo';
 
 function App() {
   return (
     <div className="App">
-      <DemoGrid />
+      {/* <DemoGrid /> */}
+      <PaddingMarginDemo />
     </div>
   );
 }
