9c23b096ad4f57f1798dbc7b2edc4348a6780186
Nhi Truong <tamnhi96@gmail.com>
2022-09-20 18:46:27 -0700

    tailwindcss_border-radius

---
+++ src/TailWindComponent/BorderRadiusDemo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function BorderRadiusDemo() {
+    return (
+        <div className="container mt-5">
+            <div className="rounded-full bg-green-400 text-white p-5 w-20 h-20">
+                BorderRadiusDemo
+            </div>
+        </div>
+    )
+}
