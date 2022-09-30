a96baf31cff3e225489403f2039278a2c3b0b803
Nhi Truong <tamnhi96@gmail.com>
2022-09-29 23:03:13 -0700

    update_branch_movie

--- src/App.js
+++ src/App.js
@@ -1,44 +1,9 @@
 import './App.css';
-import ChunkLodash from './Lodash/ChunkLodash';
-import CompareObjectArray from './Lodash/CompareObjectArray';
-import FillLodash from './Lodash/FillLodash';
-import FlattenLodash from './Lodash/FlattenLodash';
-import Includes from './Lodash/IncludesLodash';
-import JoinDemo from './Lodash/JoinDemo';
-import LastFirstLodash from './Lodash/LastFirstLodash';
-import SortLodash from './Lodash/SortLodash';
-import UniqLodash from './Lodash/UniqLodash';
-import BaiTapLayoutTailwindcss from './TailWindComponent/BaiTapLayoutTailwindcss';
-import BorderRadiusDemo from './TailWindComponent/BorderRadiusDemo';
-import CustomCss from './TailWindComponent/CustomCss';
-import DemoGrid from './TailWindComponent/DemoGrid';
-import FlexDemo from './TailWindComponent/FlexDemo';
-import PaddingMarginDemo from './TailWindComponent/PaddingMarginDemo';
-import ResponsiveDemo from './TailWindComponent/ResponsiveDemo';
-import TextBackGroundDemo from './TailWindComponent/TextBackGroundDemo';
-import WidthHeightDemo from './TailWindComponent/WidthHeightDemo';
 
 function App() {
   return (
     <div className="App">
-      {/* <DemoGrid /> */}
-      {/* <PaddingMarginDemo /> */}
-      {/* <WidthHeightDemo /> */}
-      {/* <FlexDemo /> */}
-      {/* <TextBackGroundDemo /> */}
-      {/* <BorderRadiusDemo /> */}
-      {/* <ResponsiveDemo /> */}
-      {/* <CustomCss /> */}
-      {/* <BaiTapLayoutTailwindcss /> */}
-      {/* <JoinDemo /> */}
-      {/* <LastFirstLodash /> */}
-      {/* <ChunkLodash /> */}
-      {/* <FillLodash /> */}
-      {/* <SortLodash /> */}
-      {/* <Includes /> */}
-      {/* <UniqLodash /> */}
-      {/* <FlattenLodash /> */}
-      <CompareObjectArray />
+
     </div>
   );
 }
