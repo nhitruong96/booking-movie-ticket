2d4144cc8616f4438229ba68f02dbe17bf12d90b
Nhi Truong <tamnhi96@gmail.com>
2022-10-03 15:41:56 -0700

    Create Footer template

--- src/pages/Home/HomeMenu/HomeMenu.js
+++ src/pages/Home/HomeMenu/HomeMenu.js
@@ -21,10 +21,10 @@ export default function HomeMenu(props) {
                 <TabPane tab={<img src="https://picsum.photos/200" className="rounded-full" width="50" />} key="1">
                     Content of Tab 1
                 </TabPane>
-                <TabPane tab={<img src="https://picsum.photos/200" className="rounded-full" width="50" />} key="2">
+                <TabPane tab={<img src="https://picsum.photos/300" className="rounded-full" width="50" />} key="2">
                     Content of Tab 2
                 </TabPane>
-                <TabPane tab={<img src="https://picsum.photos/200" className="rounded-full" width="50" />} key="3">
+                <TabPane tab={<img src="https://picsum.photos/400" className="rounded-full" width="50" />} key="3">
                     Content of Tab 3
                 </TabPane>
             </Tabs>
--- src/templates/HomeTemplate/HomeTemplate.js
+++ src/templates/HomeTemplate/HomeTemplate.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import { Route } from "react-router-dom";
+import Footer from "./Layout/Footer/Footer";
 import Header from "./Layout/Header/Header";
 import HomeCarousel from "./Layout/HomeCarousel/HomeCarousel";
 
@@ -15,10 +16,9 @@ export const HomeTemplate = (props) => { //props: path, exact, Component
             <HomeCarousel {...propsRoute} />
             
             <Component {...propsRoute} />
-
-            <footer className="bg-black h-10 text-white">
-                This is footer homepage
-            </footer>
+            
+            <hr className="mt-2"/>
+            <Footer {...propsRoute} />
         </Fragment>
     }} />
 }
