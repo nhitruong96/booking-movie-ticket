8f4b67cd82ef574c93373d4d7522b3aaa1c0314b
Nhi Truong <tamnhi96@gmail.com>
2022-10-03 17:25:39 -0700

    Carousel_create-carouselReducer-and-connect-to-HomeCarousel

--- src/templates/HomeTemplate/Layout/HomeCarousel/HomeCarousel.js
+++ src/templates/HomeTemplate/Layout/HomeCarousel/HomeCarousel.js
@@ -1,37 +1,37 @@
 import React from 'react'
 import { Carousel } from 'antd';
+import { useSelector } from 'react-redux';
 
 const contentStyle = {
     height: '400px',
     color: '#fff',
     lineHeight: '160px',
     textAlign: 'center',
-    background: '#364d79',
+    backgroundPosition: 'center',
+    backgroundSize: '100%',
+    backgroundRepeat: 'no-repeat',
 };
 
-export default function HomeCarousel() {
+export default function HomeCarousel(props) {
+
+    const { arrImg } = useSelector(state => state.CarouselReducer)
+
+    console.log('arrImg', arrImg);
+
+    const renderImg = () => {
+        return arrImg.map((item, index) => {
+            return <div key={index}>
+                <div style={{ ...contentStyle, backgroundImage: `url(${item.hinhAnh})` }}>
+                    <img src={item.hinhAnh} className="w-full opacity-0" alt={item.maPhim} />
+                </div>
+            </div>
+        })
+    }
+
     return (
         <Carousel effect="fade">
-            <div>
-                <div style={contentStyle}>
-                    <img src="https://picsum.photos/500" className="w-full" alt="pic1" />
-                </div>
-            </div>
-            <div>
-                <div style={contentStyle}>
-                    <img src="https://picsum.photos/1000" className="w-full" alt="pic1" />
-                </div>
-            </div>
-            <div>
-                <div style={contentStyle}>
-                    <img src="https://picsum.photos/1500" className="w-full" alt="pic1" />
-                </div>
-            </div>
-            <div>
-                <div style={contentStyle}>
-                    <img src="https://picsum.photos/2000" className="w-full" alt="pic1" />
-                </div>
-            </div>
+            {renderImg()}
+
         </Carousel>
     )
 }
