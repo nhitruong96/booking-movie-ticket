5973a28ecaa4ac9e92f46f242ceed38a7ffdd41c
Nhi Truong <tamnhi96@gmail.com>
2022-10-04 19:12:41 -0700

    Organize film service, film reducer, add to redux store, and render to home page

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,10 +1,13 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import { CarouselReducer } from './reducers/CarouselReducer';
+import { FilmManagementReducer } from './reducers/FilmManagementReducer';
+
 
 const rootReducer = combineReducers({
     // state application
-    CarouselReducer // CarouselReducer: CarouselReducer
+    CarouselReducer, // CarouselReducer: CarouselReducer
+    FilmManagementReducer
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
