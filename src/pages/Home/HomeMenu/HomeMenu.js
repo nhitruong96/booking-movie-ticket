c31b7868eb150b434689de4f8ab4ffb3a280ca7e
Nhi Truong <tamnhi96@gmail.com>
2022-10-07 00:07:07 -0700

    Render Theaters list, Theater, Movie and Showtime to HomeMenu.js

--- src/pages/Home/HomeMenu/HomeMenu.js
+++ src/pages/Home/HomeMenu/HomeMenu.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { useState } from 'react';
 import { Radio, Space, Tabs } from 'antd';
+import { Fragment } from 'react';
+import { NavLink } from 'react-router-dom';
+import moment from 'moment';
 
 const { TabPane } = Tabs;
 
 export default function HomeMenu(props) {
+
     const [state, setState] = useState({
         tabPosition: 'left',
     })
@@ -17,18 +21,53 @@ export default function HomeMenu(props) {
 
     console.log(props, 'propsTheaterList')
 
+    const renderTheaterList = () => {
+        return props.arrTheater?.map((theaterBrand, index) => {
+            return <TabPane tab={<img src={theaterBrand.logo} className="rounded-full" width="50" />} key={index}>
+                <Tabs tabPosition={tabPosition}>
+                    {theaterBrand.lstCumRap?.map((theater, index) => {
+                        return <TabPane tab={
+                            <div style={{ width: '300px', display: 'flex' }}>
+                                <img src="http://picsum.photos/100" width="50" />
+                                <br />
+                                <div className="text-left ml-2">
+                                    {theater.tenCumRap}
+                                    <p className="text-red-200">Detail</p>
+                                </div>
+                            </div>
+                        } key={index}>
+                            {/* Load film according to theater */}
+                            {theater.danhSachPhim.slice(0, 4).map((film, index) => {
+                                return <Fragment key={index}>
+                                    <div className="my-5" style={{ display: 'flex' }}>
+                                        <img src={film.hinhAnh} alt={film.tenPhim} style={{ height: 75, width: 75 }} 
+                                        onError={(e) => { e.target.onerror = null; e.target.src = "https://picsum.photos/75/75" }} />
+                                        <div className="ml-2">
+                                            <h1 className="text-xl text-green-700">{film.tenPhim}</h1>
+                                            <p>{theater.diaChi}</p>
+                                            <div className="grid grid-cols-6 gap-10">
+                                                {film.lstLichChieuTheoPhim?.slice(0, 12).map((showtime, index) => {
+                                                    return <NavLink to="/" className="text-lg text-green-400" key={index}>
+                                                        {moment(showtime.ngayChieuGioChieu).format('hh:mm A')}
+                                                    </NavLink>
+                                                })}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                </Fragment>
+                            })}
+                        </TabPane>
+                    })}
+                </Tabs>
+            </TabPane>
+        })
+    }
+
     return (
         <div>
             <Tabs tabPosition={tabPosition}>
-                <TabPane tab={<img src="https://picsum.photos/200" className="rounded-full" width="50" />} key="1">
-                    Content of Tab 1
-                </TabPane>
-                <TabPane tab={<img src="https://picsum.photos/300" className="rounded-full" width="50" />} key="2">
-                    Content of Tab 2
-                </TabPane>
-                <TabPane tab={<img src="https://picsum.photos/400" className="rounded-full" width="50" />} key="3">
-                    Content of Tab 3
-                </TabPane>
+                {renderTheaterList()}
             </Tabs>
         </div>
     )
