dbfdd2a1baa7722fc5bc3d8776793b6e3a13e785
Nhi Truong <tamnhi96@gmail.com>
2022-10-08 23:29:41 -0700

    Create Login page using tailwindcss login template

--- src/templates/CheckoutTemplate/CheckoutTemplate.js
+++ src/templates/CheckoutTemplate/CheckoutTemplate.js
@@ -3,16 +3,17 @@ import { Redirect, Route } from "react-router-dom";
 import { USER_LOGIN } from "../../util/settings/config"
 
 const CheckoutTemplate = (props) => { //props: path, exact, Component
+
     const { Component, ...restProps } = props;
+
+    if (!localStorage.getItem(USER_LOGIN)) {
+        return <Redirect to='/login' />
+    }
 
     return <Route {...restProps} render={(propsRoute) => {
         //props.location.props
         //props.history.props
         //props.match.param
-
-        if (!localStorage.getItem(USER_LOGIN)) {
-            return <Redirect to='/login' />
-        }
 
         return <Fragment>
             <Component {...propsRoute} />
