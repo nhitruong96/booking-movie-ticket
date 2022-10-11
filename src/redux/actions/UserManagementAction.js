067e777a98c4b7182d0c2116c5fa60ca9f7a65f9
Nhi Truong <tamnhi96@gmail.com>
2022-10-10 22:47:04 -0700

    Create Checkout template

--- src/redux/actions/UserManagementAction.js
+++ src/redux/actions/UserManagementAction.js
@@ -1,20 +1,24 @@
 import { userMngService } from "../../services/UserManagementService"
 import { LOGIN_ACTION } from "./types/UserManagementType";
+import { history } from "../../App"
 
 export const loginAction = (loginInfo) => {
+
     return async (dispatch) => {
         try {
 
             //Getting data from API
             const result = await userMngService.login(loginInfo);
-            
+
             //After getting data from API => redux {reducer}
             // if (result.data.statusCode === 200) {
             if (result.status === 200) {
                 dispatch({
                     type: LOGIN_ACTION,
                     userLoginInfo: result.data.content
-                })
+                });
+                // Redirect after login successfully to previous page
+                history.goBack();
             }
 
             console.log('resultUserLogin', result)
