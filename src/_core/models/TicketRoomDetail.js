63d2dc3040a084b25785b690f5972cc88920fea5
Nhi Truong <tamnhi96@gmail.com>
2022-10-11 19:06:06 -0700

    Render seats from API to Checkout.js

---
+++ src/_core/models/TicketRoomDetail.js
@@ -0,0 +1,26 @@
+export class TicketRoomDetail {
+    thongTinPhim = new MovieInfo();
+    danhSachGhe = [];
+}
+
+export class MovieInfo {
+    maLichChieu = '';
+    tenCumRap = '';
+    tenRap = '';
+    diaChi = '';
+    tenPhim = '';
+    hinhAnh = '';
+    ngayChieu = '';
+    gioChieu = '';
+}
+
+export class SeatInfo {
+    maGhe = '';
+    tenGhe = '';
+    maRap = '';
+    loaiGhe = '';
+    stt = '';
+    giaVe = '';
+    daDat = '';
+    taiKhoanNguoiDat = '';
+}
--- src/util/settings/config.js
+++ src/util/settings/config.js
@@ -4,6 +4,6 @@ export const DOMAIN = 'https://movieapi.cyberlearn.vn';
 
 export const TOKEN = 'accessToken';
 
-export const GROUPID = 'GP01';
+export const GROUPID = 'GP00';
 
 export const USER_LOGIN = 'USER_LOGIN';
