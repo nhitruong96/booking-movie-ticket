c7385a82017d577b7dc9a16f2b79d4c090b74fdc
Nhi Truong <tamnhi96@gmail.com>
2022-10-11 22:27:30 -0700

    Select and unselect seats, calculate total fee

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -5,12 +5,14 @@ import { getTicketRoomDetailAction } from '../../redux/actions/BookingManagement
 import style from './Checkout.module.css'
 import './Checkout.css'
 import { CloseOutlined } from '@ant-design/icons'
+import { BOOK_TICKET } from '../../redux/actions/types/BookingManagementType';
+import _ from 'lodash';
 
 export default function Checkout(props) {
 
   const { userLogin } = useSelector(state => state.UserManagementReducer);
 
-  const { ticketRoomDetail } = useSelector(state => state.BookingManagementReducer);
+  const { ticketRoomDetail, seatBookingList } = useSelector(state => state.BookingManagementReducer);
   // console.log('ticketRoomDetail', ticketRoomDetail);
 
   const dispatch = useDispatch();
@@ -33,9 +35,22 @@ export default function Checkout(props) {
 
       let classSeatVip = seat.loaiGhe === 'Vip' ? 'seatVip' : '';
       let classSeatBooked = seat.daDat === true ? 'seatBooked' : '';
+      let classVipBooked = seat.daDat === true ? 'seatVipBooked' : '';
+
+      let classSeatBooking = '';
+      //Check each seat render to see whether it's in the seat booking list
+      let indexSeatBooking = seatBookingList.findIndex(seatBooking => seatBooking.maGhe === seat.maGhe);
+      if (indexSeatBooking !== -1) {
+        classSeatBooking = 'seatBooking';
+      }
 
       return <Fragment key={index}>
-        <button disable={seat.daDat} className={`seat ${classSeatVip} ${classSeatBooked} text-center`}>
+        <button onClick={() => {
+          dispatch({
+            type: BOOK_TICKET,
+            seatSelected: seat
+          })
+        }} disable={seat.daDat} className={`seat ${classSeatVip} ${classVipBooked} ${classSeatBooked} ${classSeatBooking} text-center`}>
           {seat.daDat ? <CloseOutlined style={{marginBottom: 7.5, fontWeight: 'bold'}} /> : seat.stt}
         </button>
         {(index + 1) % 16 === 0 ? <br /> : ''}
@@ -59,7 +74,9 @@ export default function Checkout(props) {
           </div>
         </div>
         <div className="col-span-3">
-          <h3 className="text-green-400 text-center text-2xl">0 USD</h3>
+          <h3 className="text-green-400 text-center text-2xl">Total ${seatBookingList.reduce((total, seat, index) => {
+                  return total = seat.giaVe/24000;
+                }, 0).toLocaleString()}</h3>
           <hr />
           <h3 className="text-xl mt-2">{thongTinPhim.tenPhim}</h3>
           <p>Address: {thongTinPhim.diaChi} - {thongTinPhim.tenRap}</p>
@@ -67,10 +84,17 @@ export default function Checkout(props) {
           <hr />
           <div className="flex flex-row my-5">
             <div className="w-4/5">
-              <span className="text-red-400 text-lg">Seat</span>
+              <span className="text-green-400 text-lg">Seat</span>
+              {_.sortBy(seatBookingList, ['stt']).map((seatBooking, index) => {
+                return <span key={index} className="text-red-400 text-xl"> {seatBooking.stt}</span>
+              })}
             </div>
             <div className="text-right col-span-1">
-              <span className="text-green-800 text-lg">0 USD</span>
+              <span className="text-green-500 text-lg">$
+                {seatBookingList.reduce((total, seat, index) => {
+                  return total = seat.giaVe/24000;
+                }, 0).toLocaleString()}
+              </span>
             </div>
           </div>
           <hr />
@@ -84,7 +108,7 @@ export default function Checkout(props) {
             {userLogin.soDT}
           </div>
           <hr />
-          <div className="mb-0 h-full flex flex-col justify-end items-center" style={{ marginBottom: 0 }}>
+          <div className="mb-0 h-full flex flex-col items-center" style={{ marginBottom: 0 }}>
             <div className="bg-green-500 text-white w-full text-center py-3 front-bold text-2xl">
               Book Ticket
             </div>
--- src/redux/reducers/BookingManagementReducer.js
+++ src/redux/reducers/BookingManagementReducer.js
@@ -1,16 +1,35 @@
-import { SET_TICKET_ROOM_DETAIL } from "../actions/types/BookingManagementType";
+import { BOOK_TICKET, SET_TICKET_ROOM_DETAIL } from "../actions/types/BookingManagementType";
 import { TicketRoomDetail } from "../../_core/models/TicketRoomDetail";
 
 const stateDefault = {
-    ticketRoomDetail: new TicketRoomDetail()
+    ticketRoomDetail: new TicketRoomDetail(),
+    seatBookingList: [{ giaVe: 75000, loaiGhe: "Thuong", maGhe: 47450, maRap: 451, stt: "50", tenGhe: "50" }]
 }
 
 export const BookingManagementReducer = (state = stateDefault, action) => {
     switch (action.type) {
 
-        case SET_TICKET_ROOM_DETAIL:
+        case SET_TICKET_ROOM_DETAIL: {
             state.ticketRoomDetail = action.ticketRoomDetail;
             return { ...state };
+        }
+
+        case BOOK_TICKET: {
+            // console.log(action);
+
+            //Update list of seats currently booking
+            let updateSeatBookingList = [...state.seatBookingList];
+
+            let index = updateSeatBookingList.findIndex(seatBooking => seatBooking.maGhe === action.seatSelected.maGhe);
+            if (index !== -1) {
+                //If a seat is found in the list meaning it was selected before => unselect it (by removing it)
+                updateSeatBookingList.splice(index, 1);
+            } else {
+                updateSeatBookingList.push(action.seatSelected);
+            }
+            return { ...state, seatBookingList: updateSeatBookingList }
+        }
+
 
         default: return { ...state }
     }
